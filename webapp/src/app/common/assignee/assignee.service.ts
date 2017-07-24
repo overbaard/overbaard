c25744d00eafe4524797ff4024e29f9eb82ec895
Kabir Khan <kkhan@redhat.com>
2017-07-24 19:39:38 +0200

    Use withMutations

--- webapp/src/app/common/assignee/assignee.service.ts
+++ webapp/src/app/common/assignee/assignee.service.ts
@@ -34,9 +34,11 @@ export function reducer(state: AssigneeState = initialState, action: Action): As
     case ADD_ASSIGNEES: {
       const payload: Assignee[] = (<AddAssigneesAction>action).payload;
       let assignees = state.assignees;
-      for (const assignee of payload) {
-        assignees = assignees.set(assignee.key, assignee);
-      }
+      assignees = assignees.withMutations(mutable => {
+        for (const assignee of payload) {
+          mutable.set(assignee.key, assignee);
+        }
+      });
       assignees = <Immutable.OrderedMap<string, Assignee>>assignees.sort(
         (valueA, valueB) => valueA.name.toLocaleLowerCase().localeCompare(valueB.name.toLocaleLowerCase()));
       return {
@@ -80,51 +82,3 @@ export class AssigneesService {
     this.store.dispatch(new ClearAssigneesAction());
   }
 }
-
-
-/*
-const initialAssignees = Immutable.OrderedMap<string, Assignee>();
-
-export const assignees: ActionReducer<Immutable.OrderedMap<string, Assignee>> =
-  (state: Immutable.OrderedMap<string, Assignee> = initialAssignees, action: Action) => {
-
-    switch (action.type) {
-      case ADD_ASSIGNEES: {
-        const payload: Assignee[] = <Assignee[]>action.payload;
-        for (const assignee of payload) {
-          state = state.set(assignee.key, assignee);
-        }
-        state = <Immutable.OrderedMap<string, Assignee>>state.sort(
-          (valueA, valueB) => valueA.name.toLocaleLowerCase().localeCompare(valueB.name.toLocaleLowerCase()));
-        return state;
-      }
-      case CLEAR_ASSIGNEES: {
-        return state.clear();
-      }
-      default:
-        return state;
-    }
-  };
-
-@Injectable()
-export class AssigneesService {
-  constructor(private store: Store<AppStore>) {
-  }
-
-  getAssignees(): Observable<Immutable.OrderedMap<string, Assignee>> {
-    return this.store.select('assignees');
-  }
-
-  addAssignee(assignee: Assignee) {
-    this.store.dispatch({type: ADD_ASSIGNEES, payload: [assignee]});
-  }
-
-  addAssignees(...assigneesArr: Assignee[]) {
-    this.store.dispatch({type: ADD_ASSIGNEES, payload: assigneesArr});
-  }
-
-  clearAssignees() {
-    this.store.dispatch({type: CLEAR_ASSIGNEES, payload: null});
-  }
-}
-*/
