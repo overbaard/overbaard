de42da380ab732302f67349f36811e27f6fc48d9
Kabir Khan <kkhan@redhat.com>
2017-09-28 18:09:57 +0200

    Experiment with setting the values on the components to get the unit tests passing

--- webapp/src/app/components/board/view/kanban-view/kanban-view-headers/kanban-view-header.component.spec.ts
+++ webapp/src/app/components/board/view/kanban-view/kanban-view-headers/kanban-view-header.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { KanbanViewHeaderComponent } from './kanban-view-header.component';
 import {KanbanViewHeaderRowComponent} from './kanban-view-header-row.component';
 import {KanbanViewComponent} from '../kanban-view.component';
+import {List} from 'immutable';
 
 describe('KanbanViewHeaderComponent', () => {
   let component: KanbanViewHeaderComponent;
@@ -19,10 +20,23 @@ describe('KanbanViewHeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(KanbanViewHeaderComponent);
     component = fixture.componentInstance;
+    component.header = {
+      name: 'Test',
+      abbreviated: '',
+      rows: 1,
+      cols: 1,
+      wip: 0,
+      backlog: false,
+      states: List<number>()
+    }
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be created', () => {
+    expect(component.header).toBeTruthy();
   });
 });
