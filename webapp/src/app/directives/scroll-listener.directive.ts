3356db65c8a972eedd542b945bf3bde9fa22d293
Kabir Khan <kkhan@redhat.com>
2018-01-27 14:53:16 +0000

    Improve scrolling performance by running outside NgZone

--- webapp/src/app/directives/scroll-listener.directive.ts
+++ webapp/src/app/directives/scroll-listener.directive.ts
@@ -1,4 +1,8 @@
-import {Directive, ElementRef, EventEmitter, HostListener, OnDestroy, OnInit, Output} from '@angular/core';
+import {
+  AfterViewInit,
+  Directive, ElementRef, EventEmitter, HostListener, NgZone, OnDestroy, OnInit, Output,
+  Renderer2
+} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/throttleTime';
@@ -6,60 +10,42 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/fromEvent';
 import {Observable} from 'rxjs/Observable';
 
 
 @Directive({
-  selector: '[scroll-listener]'
+  selector: '[appScrollListener]'
 })
-export class ScrollListenerDirective implements OnInit, OnDestroy{
+export class ScrollListenerDirective implements OnInit, OnDestroy, AfterViewInit {
   @Output()
   scrollLeft: EventEmitter<number> = new EventEmitter<number>();
 
-  private scrollSubject$: Subject<number> = new BehaviorSubject(0);
-  private destroy$: Subject<void> = new Subject<void>();
+  private destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private _ref: ElementRef) {
+  constructor(private _ref: ElementRef,
+              private readonly _renderer: Renderer2,
+              private readonly _zone: NgZone) {
   }
 
   ngOnInit(): void {
-    this.scrollSubject$
-      .asObservable()
-      .throttleTime(25)
-      .merge(
-        // If just handling the scroll event, we miss out on some of the last values due to the throttleTime
-        // so periodically ping here
-        Observable.timer(0, 1000)
-          .map(data => this._ref.nativeElement.scrollLeft)
-      )
-      .takeUntil(this.destroy$)
-      .distinctUntilChanged()
-      .subscribe(
-        scroll => {
-          // console.log('Subject:  ' + scroll);
-          this.scrollLeft.next(scroll * -1);
-        }
-      );
 
-    // Trigger this every so often since the original
-    Observable.timer(0, 80)
-      .takeUntil(this.destroy$)
-      .subscribe(
-        scroll => {
-        this.scrollSubject$.next(this._ref.nativeElement.scrollLeft)
-      });
+  }
 
+  ngAfterViewInit(): void {
+    this._zone.runOutsideAngular(() => {
+      Observable.fromEvent(this._ref.nativeElement, 'scroll')
+        .takeUntil(this.destroy$)
+        .debounceTime(20)
+        .subscribe(
+          res => {
+            const left: number = this._ref.nativeElement.scrollLeft;
+              this._zone.run(() => this.scrollLeft.next(left * -1))
+          });
+    });
   }
 
   ngOnDestroy(): void {
-  }
-
-  @HostListener('scroll') onScroll() {
-    const offset: number = this._ref.nativeElement.scrollLeft;
-    // console.log('Scroll: ' + offset);
-
-    this.scrollSubject$.next(offset)
-
-
+    this.destroy$.next(true);
   }
 }
